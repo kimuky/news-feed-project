87f68b00dde29716a1175811fb73465b5c537468
heroKim997 <ghkufud6545@gmail.com>
2024-11-20 18:01:04 +0900

    test

---
+++ src/main/java/com/example/newsfeedproject/controller/PostController.java
@@ -0,0 +1,18 @@
+package com.example.newsfeedproject.controller;
+
+
+import com.example.newsfeedproject.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostRepository postRepository;
+    private final UserService userService;
+
+    //test
+
+
+}
