ec2dd9fd4bda2021a4741acadc9f99177c7b2f9c
heroKim997 <ghkufud6545@gmail.com>
2024-11-21 10:14:33 +0900

    - 게시물 작성 수정 삭제 개발 완료

--- src/main/java/com/example/newsfeedproject/controller/PostController.java
+++ src/main/java/com/example/newsfeedproject/controller/PostController.java
@@ -3,17 +3,17 @@ package com.example.newsfeedproject.controller;
 
 import com.example.newsfeedproject.dto.post.PostRequestDto;
 import com.example.newsfeedproject.dto.post.PostResponseDto;
+import com.example.newsfeedproject.dto.post.PostUpdateRequestDto;
+import com.example.newsfeedproject.dto.post.PostUpdateResponseDto;
 import com.example.newsfeedproject.service.PostService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,7 +24,7 @@ public class PostController {
 
 
     //작성
-    @PostMapping()
+    @PostMapping
     public ResponseEntity<PostResponseDto> createPost(@RequestBody PostRequestDto postRequestDto, HttpServletRequest servletRequest) {
         HttpSession session = servletRequest.getSession();
         String email = String.valueOf(session.getAttribute("email"));
@@ -34,8 +34,22 @@ public class PostController {
 
 
     //수정
+    @PatchMapping("/{id}")
+    public ResponseEntity<PostUpdateResponseDto> updatePost(@RequestBody PostUpdateRequestDto postUpdateRequestDto, @PathVariable Long id, HttpServletRequest servletRequest) {
+        HttpSession session = servletRequest.getSession();
+        String email = String.valueOf(session.getAttribute("email"));
+
+        return ResponseEntity.ok().body(postService.updatePost(id, postUpdateRequestDto, email));
+    }
 
     //삭제
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable Long id, HttpServletRequest servletRequest) {
+        HttpSession session = servletRequest.getSession();
+        String email = String.valueOf(session.getAttribute("email"));
+        postService.deletePost(id, email);
+        return ResponseEntity.ok().body("정상적으로 삭제되었습니다.");
+    }
 
 
 }
