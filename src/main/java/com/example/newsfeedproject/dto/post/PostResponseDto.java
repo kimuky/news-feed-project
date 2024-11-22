7a87f47b84d2aff89fc87c41d7792b514ed415a1
jiyoon <rlawldbs9128@gmail.com>
2024-11-22 20:32:11 +0900

    fix, update : String -> LocalDateTime, updateDate 추가

--- src/main/java/com/example/newsfeedproject/dto/post/PostResponseDto.java
+++ src/main/java/com/example/newsfeedproject/dto/post/PostResponseDto.java
@@ -3,6 +3,8 @@ package com.example.newsfeedproject.dto.post;
 import com.example.newsfeedproject.entity.Post;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class PostResponseDto {
     private Long id;
@@ -13,7 +15,9 @@ public class PostResponseDto {
 
     private String writerName; //작성자 이름
 
-    private String createdDate; //작성일
+    private LocalDateTime createdDate; //작성일
+
+    private LocalDateTime updatedDate;
 
     private int likeCount;
 
@@ -24,7 +28,8 @@ public class PostResponseDto {
         dto.title = post.getTitle();
         dto.content = post.getContent();
         dto.writerName = post.getUser().getName(); //UserEntity에서 이름 가져오기
-        dto.createdDate = post.getCreatedAt().toString(); //작성일을 문자열로 변환
+        dto.createdDate = post.getCreatedAt();
+        dto.updatedDate = post.getUpdatedAt();
         dto.likeCount = post.getLikeCount();
 
         return dto;
