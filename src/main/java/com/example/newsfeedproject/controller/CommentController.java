b1c5425fe25cccd274eb4d3ca04d62c69a5db467
김도균 <dogyoun1110@gmail.com>
2024-11-22 16:31:40 +0900

    fix: not blank 2

--- src/main/java/com/example/newsfeedproject/controller/CommentController.java
+++ src/main/java/com/example/newsfeedproject/controller/CommentController.java
@@ -4,16 +4,11 @@ import com.example.newsfeedproject.dto.comment.CommentRequestDto;
 import com.example.newsfeedproject.dto.comment.CommentResponseDto;
 import com.example.newsfeedproject.dto.comment.CommentUpdateRequestDto;
 import com.example.newsfeedproject.dto.comment.CommentUpdateResponseDto;
-import com.example.newsfeedproject.dto.post.PostUpdateRequestDto;
-import com.example.newsfeedproject.entity.Post;
 import com.example.newsfeedproject.service.CommentService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.query.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -45,7 +40,7 @@ public class CommentController {
 
     //댓글 수정
     @PatchMapping("/{commentId}")
-    public ResponseEntity<CommentUpdateResponseDto> updateComment(@RequestBody CommentUpdateRequestDto commentUpdateRequestDto,
+    public ResponseEntity<CommentUpdateResponseDto> updateComment(@Valid @RequestBody CommentUpdateRequestDto commentUpdateRequestDto,
                                                                   @PathVariable Long commentId, HttpServletRequest servletRequest) {
         HttpSession session = servletRequest.getSession();
         String email = String.valueOf(session.getAttribute("email"));
